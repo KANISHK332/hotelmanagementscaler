f635a99669fb0e9da852a35fcf639565f6212447
KANISHK332 <rawatkanishk123@gmail.com>
2023-10-10 21:02:30 +0530

    updated email

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -2,20 +2,20 @@ const nodeMailer = require("nodemailer");
 
 const sendEmail = async (options) => {
     const transporter = nodeMailer.createTransport({
-        // host: process.env.SMTP_HOST,
-        // port: process.env.SMTP_PORT,
-        // service: process.env.SMPT_SERVICE,
-        // // service: 'gmail',
-        // auth:{
-        //     user: process.env.SMPT_MAIL,
-        //     pass:process.env.SMPT_PASSWORD,
-        // },
-        host: "sandbox.smtp.mailtrap.io",
-        port: 2525,
-        auth: {
-            user: "270e1538c38b68",
-            pass: "b7fe1e9b9a0860"
-        }
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        service: process.env.SMPT_SERVICE,
+        // service: 'gmail',
+        auth:{
+            user: process.env.SMPT_MAIL,
+            pass:process.env.SMPT_PASSWORD,
+        },
+        // host: "sandbox.smtp.mailtrap.io",
+        // port: 2525,
+        // auth: {
+        //     user: "270e1538c38b68",
+        //     pass: "b7fe1e9b9a0860"
+        // }
     });
 
     const mailOptions = {
